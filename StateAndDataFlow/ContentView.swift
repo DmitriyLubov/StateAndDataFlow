4dd29890596b04550f770dc32ac66284c98c07b6
Dmitriy <snipere@list.ru>
2023-04-29 20:46:36 +1000

    Work with logOut

--- StateAndDataFlow/ContentView.swift
+++ StateAndDataFlow/ContentView.swift
@@ -13,17 +13,29 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            Spacer()
+            
             Text("Hi, \(userSettings.name)")
                 .font(.largeTitle)
-                .padding(.top, 100)
+            
+            Spacer()
+            
             Text(timer.counter.formatted())
                 .font(.largeTitle)
-                .padding(.top, 100)
-            Spacer()
-            
-            ButtonView(timer: timer)
             
             Spacer()
+            
+            ButtonView(title: timer.buttonTitle, color: .red) {
+                timer.startTimer()
+            }
+            .padding(.bottom, 200)
+            
+            Spacer()
+            
+            ButtonView(title: "LogOut", color: .blue) {
+                userSettings.name = ""
+                userSettings.isLoggedIn.toggle()
+            }
         }
         .padding()
     }
@@ -36,23 +48,4 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct ButtonView: View {
-    @ObservedObject var timer: TimeCounter
-    
-    var body: some View {
-        
-        Button(action: timer.startTimer) {
-            Text(timer.buttonTitle)
-                .font(.title)
-                .fontWeight(.bold)
-                .foregroundColor(.white)
-        }
-        .frame(width: 200, height: 60)
-        .background(.red)
-        .cornerRadius(20)
-        .overlay(
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(.black, lineWidth: 4)
-        )
-    }
-}
+
